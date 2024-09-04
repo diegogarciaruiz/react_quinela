c88fdda9680f666965a5ab4ae39fd08907b90a32
Pau Fàbrega Rodríguez-Roda <paufabregaroda@MBP-de-Pau.home>
2024-09-04 22:49:25 +0200

    feat: db sync

--- server/db/db.js
+++ server/db/db.js
@@ -4,12 +4,15 @@ require("dotenv").config();
 async function initDb() {
   try {
     const connection = await mysql.createConnection({
+      database: process.env.DB_NAME,
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
     });
 
-    await connection.query(`CREATE DATABASE IF NOT EXISTS quiniela`);
+    await connection.query(
+      `CREATE DATABASE IF NOT EXISTS \`${process.env.DB_NAME}\``
+    );
     console.log("Database quiniela checked/created.");
 
     // Close the connection after database is created or checked
@@ -26,13 +29,13 @@ initDb().catch((err) => {
 
 // Create a pool to manage connections to the 'quiniela' database
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "1234",
-  database: "quiniela", // Database name 'quiniela'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 10, // Max number of connections in the pool
-  queueLimit: 0, // No limit to the number of queued connection requests
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 // Export the pool for use elsewhere in the application
